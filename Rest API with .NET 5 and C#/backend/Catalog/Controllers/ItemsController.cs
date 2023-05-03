74fc30b6dd4c1a7a7a29861795bcba889e62142d
zabinskas.paulius951@gmail.com <zabinskas.paulius951@gmail.com>
2023-05-03 22:12:04 +0100

    DI, Dtos added

--- Rest API with .NET 5 and C#/backend/Catalog/Controllers/ItemsController.cs
+++ Rest API with .NET 5 and C#/backend/Catalog/Controllers/ItemsController.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using Catalog.Dtos;
 using Catalog.Entities;
 using Catalog.Repositories;
 using Microsoft.AspNetCore.Mvc;
@@ -13,26 +15,28 @@ namespace Catalog.Controllers
     public class ItemsController : ControllerBase
     {
         // creating instance of database 
-        private readonly InMemItemsRepository repository;
-
-        public ItemsController()
+        private readonly IItemsRepository repository;
+        
+        // Passing interface
+        // Dependency injection 
+        public ItemsController(IItemsRepository repository)
         {
-            repository = new InMemItemsRepository();
+            this.repository = repository;
         }
 
         // get /items
         //Handles HTTP GET requests at the "/items" endpoint
         [HttpGet]
-        public IEnumerable<Item> GetItems()
+        public IEnumerable<ItemDto> GetItems()
         {
-            var items = repository.GetItems();
+            var items = repository.GetItems().Select( item => item.AsDto());
             return items;
         }
 
         //get /items/{id}
         // Handles HTTP GET requests at the "/items/{id}" endpoint, where {id} is a placeholder for the item ID
         [HttpGet("{id}")]
-        public ActionResult<Item> GetItem(Guid id) // action return allows to return more than one data type
+        public ActionResult<ItemDto> GetItem(Guid id) // action return allows to return more than one data type
         {
             var item = repository.GetItem(id);
 
@@ -41,7 +45,7 @@ namespace Catalog.Controllers
                 return NotFound();
             }
 
-            return item;
+            return item.AsDto();
         }
 
     }
